27e46e6294e04c6df8ec0aea78b7ffac4de5b53e
lzuqer <lzuqer@gmail.com>
2020-07-15 10:29:37 +0800

    Update distributed_train.sh

--- examples/nas/cream/distributed_train.sh
+++ examples/nas/cream/distributed_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUM_PROC=$1
 shift
-python -m torch.distributed.launch --nproc_per_node=$NUM_PROC ./supernet.py "$@"
+python -m torch.distributed.launch --nproc_per_node=$NUM_PROC ./examples/nas/cream/supernet.py "$@"
