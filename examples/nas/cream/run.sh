461ef242d2efe915ac58cbea27049abdd4d6dc73
lzuqer <lzuqer@gmail.com>
2020-07-31 13:57:45 +0800

    Update run.sh

--- examples/nas/cream/run.sh
+++ examples/nas/cream/run.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 ./examples/nas/cream/distributed_train.sh 8 \
---data ../NIPS20_release/data/imagenet/ --sched spos_linear \
+--data ../FastBaseline/data/imagenet/ --sched spos_linear \
 --pool_size 10 --meta_sta_epoch 20 --update_iter 200 \
 --epochs 120  --batch-size 128 --warmup-epochs 0 \
 --lr 0.5  --opt-eps 0.001 \
